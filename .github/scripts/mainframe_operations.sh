03350c9494a359c604b64a0af36358908a8088e8
monday04 <50302235+monday04@users.noreply.github.com>
2026-08-19 13:13:16 +0200

    Add main COBOL Check execution script”
    This script sets up the environment for mainframe operations, checks Java availability, and runs cobolcheck for specified programs, copying the output files as needed.

---
+++ .github/scripts/mainframe_operations.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# mainframe_operations.sh
+# Setup environment
+export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
+export JAVA_HOME=/usr/lpp/java/J8.0_64
+export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
+
+# Check Java availability
+java -version
+
+#Set ZOWE_USERNAME
+ZOWE_USERNAME="Z31821"
+
+# Change to the cobolcheck directory
+cd cobolcheck
+echo "Changed to $(pwd)"
+ls -al
+
+# Make cobolcheck executable
+chmod +x cobolcheck
+echo "made cobolcheck executable"
+
+# Make script in scripts directory executable
+cd scripts
+chmod +x linux_gnucobol_run_tests
+echo "made linux_gnucobol:run_tests executable"
+cd ..
+
+# Function to run cobolcheck and copy files
+run_cobol-check() {
+  program=$1
+  echo "running cobolcheck for $program"
+
+  # Run cobolcheck, but don't exit if it fails
+  ./cobolcheck -p $program
+  echo "Cobolcheck execution completed for $program (exceptions may have occured)"
+
+  # Checj if CC##99.CBL was created, regardless of cobolcheck exit status
+  if [ -f "CC##99.CBL" ]; then
+    # copy to the mvs dataset
+    if cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL($program)'"; then
+      echo "copied CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
+    else
+      echo "failed to copy CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
+    fi
+  else
+    echo "CC##99.CBL not found for $program"
+  fi
+
+  # Copy the JCL file if it exists
+  if [ -f "${program}.JCL" ]; then
+    if cp ${program}.JCL "//'${ZOWE_USERNAME}.JCL($program)'"; then
+      echo "Copied ${program}.JCL to ${ZOWE_USERNAME}.JCL($program)"
+    else
+      echo "failed to copy ${program}.JCL to ${ZOWE_USERNAME}.JCL($program)"
+    fi
+  else
+    echo "${program}.JCL not found"
+  fi
+}
+
+# Run for each program
+for program in NUMBERS EMPPAY DEPTPAY, do
+  run_cobolcheck $program
+done
+
+echo "Mainframe operations completed"
