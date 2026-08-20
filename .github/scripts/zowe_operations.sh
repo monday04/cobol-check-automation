f971fd94d34de8993051a5325d330905106b89d5
monday04 <50302235+monday04@users.noreply.github.com>
2026-08-20 08:50:20 +0200

    Enhance zowe_operations.sh with config commands
    Added commands to list Zowe config and save it to a JSON file.

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -2,7 +2,11 @@
 # zowe_operations.sh
 # Create zowe profile
 echo "Creating zowe Profile"
-zowe config auto-init
+set -x
+zowe config list
+echo "$ZOWE_CONFIG_JSON" > zowe.config.json
+zowe config set profiles.project_base.user "$ZOWE_USERNAME"
+zowe config set profiles.project_base.password "$ZOWE_PASSWORD"
 
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
