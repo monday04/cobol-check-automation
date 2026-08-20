077e3de776bbfdd2bb8bafe81ccca38f425a03ff
monday04 <50302235+monday04@users.noreply.github.com>
2026-08-20 14:19:30 +0200

    Rename cobol-check directory to cobolcheck

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -13,16 +13,16 @@ zowe zosmf check status
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 # Check if directory exists, create if not
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobol-check" &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Directory does not exists. Creating it..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobol-check
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
 else
   echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobol-check" --recursive --binary-files "cobol-check-0.2.9.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
 
 # Verify upload
 echo "Verifying upload:"
-zowe --response-format-json false zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobol-check" || echo "Done!"
+zowe --response-format-json false zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" || echo "Done!"
